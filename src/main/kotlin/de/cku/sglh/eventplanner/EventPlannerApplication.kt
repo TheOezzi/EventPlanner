317a8854f056a577e41e0e29314955e71a7b34b2
CKu <c.kuebler@insiders-technologies.de>
2021-10-23 13:26:00 +0200

    initial spring setup

---
+++ src/main/kotlin/de/cku/sglh/eventplanner/EventPlannerApplication.kt
@@ -0,0 +1,11 @@
+package de.cku.sglh.eventplanner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EventPlannerApplication
+
+fun main(args: Array<String>) {
+	runApplication<EventPlannerApplication>(*args)
+}
---
+++ src/test/kotlin/de/cku/sglh/eventplanner/EventPlannerApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.cku.sglh.eventplanner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EventPlannerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
